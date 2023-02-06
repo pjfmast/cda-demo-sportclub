fc04e1afb296d8c2308f8449a2f4328beabce4da
Paul de Mast <pjfmast@gmail.com>
2023-02-06 13:51:59 +0100

    move demo extension method to project IntroCSharp

--- IntroCSharp/Program.cs
+++ IntroCSharp/Program.cs
@@ -1,5 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using SportClub.API.Entities;
+using SportClub.API.Extensions;
 
 Console.WriteLine("Hello, World!");
 
@@ -7,3 +8,6 @@ DateTime dt = DateTime.Now;
 string formatedDT = dt.ToString("HH:mm");
 
 Console.WriteLine($"Now is: {formatedDT}");
+
+10.Times(s => Console.WriteLine(s));
+
