8c62938feb3bb3ade9e2d285a7c4d8a5d03c5e7c
Paul de Mast <pjfmast@gmail.com>
2023-03-24 15:33:28 +0100

    Add a comment on changing dto type from record to a class

--- SportClub.Models/Dtos/LessonDto.cs
+++ SportClub.Models/Dtos/LessonDto.cs
@@ -9,6 +9,9 @@ namespace SportClub.Models.Dtos
     // beste practice: use C#9 records for DTO's.
     // see: https://stackoverflow.com/questions/64816714/when-to-use-record-vs-class-vs-struct
     // for working with record type: https://www.infoworld.com/article/3607372/how-to-work-with-record-types-in-csharp-9.html
+
+    // BUT: Blazor model binding and validation is not supported (yet)
+    //     when using a record-type, see: https://github.com/dotnet/aspnetcore/issues/31604
     public record LessonDto(
         int Id,
         string Title,
